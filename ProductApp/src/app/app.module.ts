acb684285576974f5247128ba01b0b754919bf42
AAKASH HANDA <AAKASH HANDA>
2018-06-25 21:59:43 +0530

    A

--- ProductApp/src/app/app.module.ts
+++ ProductApp/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule }  from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard.component'
@@ -9,13 +11,27 @@ import { upperValuePipe } from "./products/upperValue.pipe";
 import { AddValuePipe } from "./products/addValue.pipe";
 import { ProductFilterPipe } from "./products/productFilter.pipe";
 import { StarComponent } from "./shared/star.component";
+import { ProductService } from "./products/product.service";
+import { ProductDetail } from "./products/product_detail.component";
+import { OrderComponent } from "./orders/order.component";
+import { HomeComponent } from "./home/home.component";
+import { NotFoundComponent } from "./shared/notFound.component";
 
 @NgModule({
 
     //All Module decl here
     imports:[
         BrowserModule,
-        FormsModule
+        FormsModule,
+        HttpModule,
+        RouterModule.forRoot([
+            {path:'products', component:ProductComponent},
+            {path:'products/:id', component:ProductDetail},
+            {path:'orders', component:OrderComponent},
+            {path:'home', component:HomeComponent},
+            {path:'',redirectTo:'home',pathMatch:'full'},
+            {path:'**', component:NotFoundComponent}
+        ])
     ],
     //All Component & Pipe
     declarations:[
@@ -25,14 +41,20 @@ import { StarComponent } from "./shared/star.component";
         upperValuePipe,
         AddValuePipe,
         ProductFilterPipe,
-        StarComponent
+        StarComponent,
+        ProductDetail,
+        OrderComponent,
+        HomeComponent,
+        NotFoundComponent
     ],
     //Only First Component
     bootstrap:[
         AppComponent
     ],
     //All Services decl here
-    providers:[]
+    providers:[
+        ProductService
+    ]
 })
 
 export class AppModule{
